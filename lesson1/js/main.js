3b258c4fb054bd5f6f38ecf200fae1a80fe9ae74
Nata Malinova <rawr@raery.ru>
2021-01-18 22:06:28 +0700

    logo added, bugs fixed

--- lesson1/js/main.js
+++ lesson1/js/main.js
@@ -1,22 +1,21 @@
 const products = [
-    { id: 1, title: undefined, price: 2000 },
-    { id: 2, title: 'Keyboard', price: undefined },
+    { id: 1, title: 'Notenook', price: 2000 },
+    { id: 2, title: 'Keyboard', price: 200 },
     { id: 3, title: 'Mouse', price: 100 },
     { id: 4, title: 'Gamepad', price: 87 },
 ];
 
 
-const renderProduct = (title = 'Товар', price = 0) => (
-    `<div class="product-item">
-                <h3 class="product-title">${title}</h3>
-                <img src="../lesson1/img/placeholder.png" alt="placeholder">
-                <p  class="product-price">${price}</p>
+const renderProduct = (product, img = '../lesson1/img/placeholder.png" alt="placeholder') => {
+    return `<div class="product-item">
+                <img src="${img}" alt="${product.title}">
+                <h3 class="product-title">${product.title}</h3>
+                <p  class="product-price">${product.price}</p>
                 <button class="btn btn-buy">В корзину</button>
             </div>`
-);
+};
 
 const render = productsList => {
-    const productsElements = productsList.map(item => renderProduct(item.title, item.price));
     document.querySelector('.products').innerHTML = productsElements.join('');
 };
 
