f6ff65036e08c35d3a258715e68327bef12366f1
Nata Malinova <rawr@raery.ru>
2021-01-25 02:11:19 +0700

    добавлено динамическое получение данных и их отображение

--- lesson1/js/main.js
+++ lesson1/js/main.js
@@ -1,19 +1,42 @@
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
+
+//метод на xhr для получения данных. Нужно переделать в promise.
+//для примера, использовать не будем.
+let getData = (url, cb) => { //cb - callback
+    let xhr = new XMLHttpRequest();
+    xhr.open(GET, url, true);
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState === 4) {
+            if (xhr.status !== 200) {
+                console.log('Возникла ошибка ', xhr.status, xhr.statusText);
+            }
+            else {
+                cb(xhr.responseText);
+            }
+        }
+    }
+}
+
+
 class Product {
     constructor(product, img = '../lesson1/img/placeholder.png" alt="placeholder') {
-        let { title, price = 0, id } = product;
-        this.title = title;
+        let { product_name, price = 0, id_product } = product;
+        this.title = product_name;
         this.img = img;
         this.price = price;
-        this.id = id;
+        this.id = id_product;
+        this.rendered = false;
     }
 
     render() {
+        this.rendered = true;
         return `<div class="product-item">
-                <img src="${this.img}" alt="${this.title}">
-                <h3 class="product-title">${this.title}</h3>
-                <p  class="product-price">${this.price}</p>
-                <button class="btn btn-buy">В корзину</button>
-            </div>`;
+                    <img src="${this.img}" alt="${this.title}">
+                    <h3 class="product-title">${this.title}</h3>
+                    <p  class="product-price">${this.price}</p>
+                    <button class="btn btn-buy">В корзину</button>
+                </div>`;
     }
 }
 
@@ -22,34 +45,39 @@ class ProductsList {
         this.data = []; //хранит в себе все данные, полученные от сервера. необработанные
         this.products = []; //массив созданных на основе данных экземпляров класса Product
         this.container = document.querySelector(container); //контейнер на стр, куда отрендеренный товар добавляется
-        this._fetchData();
-        this._render();
+        this._fetchData()
+            .then(() => this._render());
+        // this._render();
     }
 
-    // getCost() {
-    //     let sum = 0;
-    //     for (let product of this.products) {
-    //         sum += product.price;
-    //     }
-    //     return sum;
-    // }
-    //в CartItem будет использоваться метод для подсчета стоимости одного вида товара (товар * количество)
-    //а в Cart  метод для подсчета общей суммы всех товаров
+    _fetchData() {
+        return fetch(`${API}/catalogData.json`)
+            .then(result => result.json())
+            .then(data => {
+                this.data = data;
+                for (let dataEl of this.data) { //проитерировать все данные, полученные от сервера
+                    const product = new Product(dataEl); //происходит создание товара, передаем кусочек данных 
+                    this.products.push(product);
+                }
+                this._render();
+            })
 
-    _fetchData() { //пока захардкожено, без сервера
-        this.data = [
-            { id: 1, title: 'Notenook', price: 2000 },
-            { id: 2, title: 'Keyboard', price: 200 },
-            { id: 3, title: 'Mouse', price: 100 },
-            { id: 4, title: 'Gamepad' },
-        ];
+
+        //без сервера:
+        // this.data = [
+        //     { id: 1, title: 'Notenook', price: 2000 },
+        //     { id: 2, title: 'Keyboard', price: 200 },
+        //     { id: 3, title: 'Mouse', price: 100 },
+        //     { id: 4, title: 'Gamepad' },
+        // ];
 
     }
 
     _render() { //рендерит товары
-        for (let dataEl of this.data) { //проитерировать все данные, полученные от сервера
-            const product = new Product(dataEl);
-            this.products.push(product);
+        for (let product of this.products) { //проитерировать все данные, полученные от сервера
+            if (product.rendered) {
+                continue;
+            }
             this.container.insertAdjacentHTML('beforeend', product.render());
         }
     }
