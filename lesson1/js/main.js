88d7188ca5ccc97505764c6d3b4b14b06b5fadd6
Nata Malinova <rawr@raery.ru>
2021-01-27 15:08:01 +0700

    update

--- lesson1/js/main.js
+++ lesson1/js/main.js
@@ -21,6 +21,7 @@ let getData = (url, cb) => { //cb - callback
     }
 }
 
+// ******************************
 
 class Product {
     constructor(product, img = '../lesson1/img/placeholder.png" alt="placeholder') {
@@ -158,29 +159,3 @@ class Cart {
     } // метод для рендера списка покупок
 
 }
-
-
-//старый код
-
-// const products = [
-//     { id: 1, title: 'Notenook', price: 2000 },
-//     { id: 2, title: 'Keyboard', price: 200 },
-//     { id: 3, title: 'Mouse', price: 100 },
-//     { id: 4, title: 'Gamepad', price: 87 },
-// ];
-
-
-// const renderProduct = (product, img = '../lesson1/img/placeholder.png" alt="placeholder') => {
-//     return `<div class="product-item">
-//                 <img src="${img}" alt="${product.title}">
-//                 <h3 class="product-title">${product.title}</h3>
-//                 <p  class="product-price">${product.price}</p>
-//                 <button class="btn btn-buy">В корзину</button>
-//             </div>`
-// };
-
-// const render = productsList => {
-//     document.querySelector('.products').innerHTML = productsElements.join('');
-// };
-
-// render(products);
