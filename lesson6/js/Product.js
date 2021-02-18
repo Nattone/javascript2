56839eb69624e46970b61fdeeffb33af3bad1739
Nata Malinova <rawr@raery.ru>
2021-02-18 20:49:26 +0700

    Update Product.js

--- lesson6/js/Product.js
+++ lesson6/js/Product.js
@@ -4,6 +4,6 @@ export const Product = {
                     <img :src="product.img" :alt="product.title">
                     <h3 class="product-title">{{product.title}}</h3>
                     <p class="product-price">{{product.price}} ₽</p>
-                    <button class="btn btn-buy" @click="$root.add(product)">Купить</button>
+                    <button class="btn btn-buy" @click="add(product)">Купить</button>
                 </div>`
 }
