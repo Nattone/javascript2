d6fd7ef4359e57da9acf3960a67f4da10fc376b4
Nata Malinova <rawr@raery.ru>
2021-02-18 20:07:28 +0700

    components Vue test

---
+++ lesson6/js/Products.js
@@ -0,0 +1,39 @@
+import { Product } from "./Product.js";
+
+export const Products = {
+    components: {
+        Product
+    },
+    data: {
+        catalogUrl: '/catalog.json',
+        products: []
+    },
+    methods: {},
+    // computed: {
+    //     filtered() {
+    //         return this.products.filter(item => new RegExp(this.search, 'i').test(item.title));
+    //     }
+    // },
+    mounted() { //?????
+        this.$root.getJson(`${this.$root.API + this.catalogUrl}`)
+            .then(data => {
+                this.data = data;
+                for (let { id_product, product_name, ...dataEl } of this.data) {
+                    const product = {
+                        id: id_product,
+                        title: product_name,
+                        ...dataEl
+                    };
+                    this.products.push(product);
+                }
+            })
+
+    },
+    template: ` <div class="products">
+                    // <Product v-for="el of filtered"></Product>
+                    <Product v-for="el of products"  
+                    :key="el.id+product"
+                    :product="el"
+                    ></Product>
+                </div>`
+}
